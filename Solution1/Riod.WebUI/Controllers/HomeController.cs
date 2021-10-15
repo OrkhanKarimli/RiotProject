7faef54d4a74d4b738e187837b1a34daa56ebb81
OrkhanKarimli <orkhansk@code.edu.az>
2021-10-15 04:28:58 +0400

    ContactUs

--- Solution1/Riod.WebUI/Controllers/HomeController.cs
+++ Solution1/Riod.WebUI/Controllers/HomeController.cs
@@ -16,5 +16,9 @@ namespace Riod.WebUI.Controllers
         {
             return View();
         }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
     }
 }
