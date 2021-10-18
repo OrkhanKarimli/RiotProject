888791b0e63ce8e2d8de44449fa74b6fe8f8d3a2
OrkhanKarimli <orkhansk@code.edu.az>
2021-10-18 18:10:14 +0400

    blog pages

---
+++ Solution1/Riod.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riod.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+    }
+}
